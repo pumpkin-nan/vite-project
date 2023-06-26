7a53e6888dc2f7de9887315790b42710f38d5109
nan gua <1317362771@qq.com>
2023-06-26 17:26:15 +0800

    062601

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
   <div class="box">
     <h1>我是根组件</h1>
+    <el-button type="primary" size="default" :icon="Plus">确认</el-button>
   </div>
 </template>
 
 <script setup lang="ts">
-const msg = "小馒头";
+import {Plus} from '@element-plus/icons-vue'
 </script>
 
 <style scoped>
-.box {
-  background: blue;
-}
+
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+// 引入element-plus插件与样式
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+// 国际化
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
-createApp(App).mount("#app");
+const app = createApp(App)
+app.use(ElementPlus, {
+    locale: zhCn,
+  })
+app.mount('#app')
+
