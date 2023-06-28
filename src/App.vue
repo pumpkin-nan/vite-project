9fec8330c4a29501408a79cb791cf8857c3dd60c
nan gua <1317362771@qq.com>
2023-06-28 16:09:06 +0800

    0628-2

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,14 @@
 </template>
 
 <script setup lang="ts">
-
+import { onMounted } from "vue";
+import {reqLogin} from './api/user';
+onMounted(()=>{
+  reqLogin({
+    username:'admin',
+    password:'111111'
+  })
+})
 </script>
 
 <style scoped lang="scss">
--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,11 +1,11 @@
-import request from '@/util/request'
+import request from '@/utils/request'
 import type { loginForm ,userResponseData,loginResponseData} from './type';
 
 enum API{
-    LOGIN_URI = '/user/login',
+    LOGIN_URL = '/user/login',
     USERINFO_URL = '/user/info'
 }
 
-export const reqLogin = (data:loginForm) =>request.post<any,loginResponseData>(API.LOGIN_URI,data)
+export const reqLogin = (data:loginForm) =>request.post<any,loginResponseData>(API.LOGIN_URL,data)
 
 export const reqInfo = ()=>request.get<any,userResponseData>(API.USERINFO_URL)
