6f483cb120f6c3f4fade6ab59725dc65d317c8c5
nan gua <1317362771@qq.com>
2023-07-05 21:19:02 +0800

    首页

--- src/components/SvgIcon/index.vue
+++ src/components/SvgIcon/index.vue
@@ -1,34 +1,31 @@
 <template>
   <div class="box">
-    <h1>svgTest</h1>
-    <svg :style="{width: width, height: height}">
-      <use :xlink:href="prefix+name" :fill="color"></use>
+    <svg :style="{ width: width, height: height }">
+      <use :xlink:href="prefix + name" :fill="color"></use>
     </svg>
   </div>
 </template>
 
 <script setup lang="ts">
 defineProps({
-  prefix:{
-    type:String,
-    default:"#icon-"
+  prefix: {
+    type: String,
+    default: "#icon-"
   },
-  name:String,
-  color:{
-    type:String,
-    default:''
+  name: String,
+  color: {
+    type: String,
+    default: ''
   },
-  width:{
-    type:String,
-    default:'16px'
+  width: {
+    type: String,
+    default: '16px'
   },
-  height:{
-    type:String,
-    default:'20px'
+  height: {
+    type: String,
+    default: '20px'
   }
 })
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,55 @@
 <template>
-  <div class="">
-    home
+  <el-card>
+    <div class="user">
+      <img :src="userStore.avatar" alt="">
+      <div class="container">
+        <h3>{{ getTime() }}好&nbsp;{{ userStore.username }}</h3>
+        <p>my space</p>
+      </div>
+    </div>
+  </el-card>
+  <div class="bottom">
+    <svgIcon name="welcome" width="600px" height="300px"></svgIcon>
   </div>
 </template>
 
 <script setup lang="ts">
+import useUserStore from '@/store/modules/user'
+import { getTime } from '@/utils/time'
+let userStore = useUserStore()
 
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.user {
+  display: flex;
+
+  img {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+  }
+
+  h3 {
+    font-size: 20px;
+    margin-bottom: 20px;
+  }
+
+  .container {
+    margin: 20px;
+
+  }
+
+  p {
+    font-style: italic;
+    color: gray;
+  }
+}
+
+.bottom {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 20px;
+}
+</style>
